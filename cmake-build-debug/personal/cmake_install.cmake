026feaa02989fab2a074ca54105f727857c11386
zym <3586149239@qq.com>
2024-11-08 15:26:37 +0800

    finish quine

--- cmake-build-debug/personal/cmake_install.cmake
+++ cmake-build-debug/personal/cmake_install.cmake
@@ -37,3 +37,8 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/software/toolbox/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/cpl-1/cmake-build-debug/personal/quine/cmake_install.cmake")
+endif()
+
