b5dabd92e09afd35d40d81874babcdec15a52bc7
zym <3586149239@qq.com>
2024-11-08 15:26:23 +0800

    finish quine

--- luogu/p1083/p1083.c
+++ luogu/p1083/p1083.c
@@ -2,31 +2,39 @@
 // Created by 35861 on 24-11-1.
 //
 #include<stdio.h>
-
-int classrooms[100001];
-
+#include <stdbool.h>
+#include <string.h>
+int n,m;
+long long int rest[100001],need[100001],r[100001],l[100001],d[100001],diff[100001];
+bool flag(const long long int x);
 int main() {
-    int n,m;
     scanf("%d %d",&n,&m);
-    for(int i=0;i<n;i++) {
-        scanf("%d",&classrooms[i]);
+    for(int i=1;i<=n;i++) {
+        scanf("%lld",&rest[i]);
     }
-    while(m--) {
-        int d,s,j;
-        int flag=0;
-        scanf("%d %d %d",&d,&s,&j);
-        for(int i=s;i<=j;i++) {
-            classrooms[i]-=d;
-            if(classrooms[i]<0) flag=1;
-        }
-        if(flag==1) break;
+    for(int i=1;i<=m;i++) {
+        scanf("%lld %lld %lld",&d[i],&l[i],&r[i]);
     }
-    if(m==0) {
-        printf("0\n");
-        return 0;
+    long long int begin=1,end=m;
+    if(flag(end)) return 0;
+    while(begin<end) {
+        long long int mid=(begin+end)/2;
+        if(flag(mid)) begin=mid+1;
+        else end=mid;
     }
-    else {
-        printf("-1\n%d\n",m+1);
-        return 0;
+    printf("-1\n%lld",begin);
+    return 0;
+}
+
+bool flag(const long long int x) {
+    memset(diff,0,sizeof(diff));
+    for(long long int i=1;i<=x;i++) {
+        diff[l[i]]+=d[i];
+        diff[r[i]+1]-=d[i];
     }
+    for(long long int i=1;i<=n;i++) {
+        need[i]=need[i-1]+diff[i];
+        if(need[i]>rest[i]) return false;
+    }
+    return true;
 }
